38c0fbe66e3764c4104412253a163ce92fb69739
Chris Berkhout <chrisberkhout@gmail.com>
2013-01-16 17:57:36 +1100

    Extracted geolocate.

---
+++ js/geolocate.js
@@ -0,0 +1,21 @@
+define(["openlayers"], function(OpenLayers) {
+
+    return new OpenLayers.Control.Geolocate({
+        id: 'locate-control',
+        geolocationOptions: {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 20000
+        },
+        failure: function(e) {
+            switch (e.error.code) {
+                case 0: alert(OpenLayers.i18n("There was an error while retrieving your location: ") + e.error.message); break;
+                case 1: alert(OpenLayers.i18n("The user didn't accept to provide the location: ")); break;
+                case 2: alert(OpenLayers.i18n("The browser was unable to determine your location: ") + e.error.message); break;
+                case 3: alert(OpenLayers.i18n("The browser timed out before retrieving the location.")); break;
+            }
+        }
+    });
+
+});
+
--- js/initMap.js
+++ js/initMap.js
@@ -4,14 +4,16 @@ define([
     "proj",
     "dataLayer",
     "bingLayers",
-    "vicmapLayers"
+    "vicmapLayers",
+    "geolocate"
 ], function(
     OpenLayers,
     PoziMap,
     proj,
     dataLayer,
     bingLayers,
-    vicmapLayers
+    vicmapLayers,
+    geolocate
 ) {
 
     return function() {
@@ -42,23 +44,6 @@ define([
         selectControl = new OpenLayers.Control.SelectFeature(dataLayer, {
             autoActivate: true,
             onSelect: onSelectFeatureFunction
-        });
-
-        var geolocate = new OpenLayers.Control.Geolocate({
-            id: 'locate-control',
-            geolocationOptions: {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 20000
-            },
-            failure: function(e) {
-                switch (e.error.code) {
-                    case 0: alert(OpenLayers.i18n("There was an error while retrieving your location: ") + e.error.message); break;
-                    case 1: alert(OpenLayers.i18n("The user didn't accept to provide the location: ")); break;
-                    case 2: alert(OpenLayers.i18n("The browser was unable to determine your location: ") + e.error.message); break;
-                    case 3: alert(OpenLayers.i18n("The browser timed out before retrieving the location.")); break;
-                }
-            }
         });
 
 
