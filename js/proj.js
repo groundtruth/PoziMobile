5fdf74da63af8d16ee3469b6dab5f532f4f9401d
Chris Berkhout <chrisberkhout@gmail.com>
2013-01-24 11:59:02 +1100

    Added some notes regarding projections (still need to corrrect variable names base on that).

--- js/proj.js
+++ js/proj.js
@@ -1,6 +1,10 @@
 define(["openlayers"], function(OpenLayers) {
     return {
+      
+        // GPS or query/response from web service
         webMercator: new OpenLayers.Projection("EPSG:4326"),
+        
+        // Google's spherical mercator, also for our tiles
         sphericalMercator: new OpenLayers.Projection("EPSG:900913")
     }
 });
