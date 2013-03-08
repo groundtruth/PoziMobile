a04cab3d59eed7617bb76b7ab4c945f39d7934d5
Chris Berkhout <chrisberkhout@gmail.com>
2013-03-08 12:52:09 +1100

    Got sync button stuff working.

--- js/pages/main.js
+++ js/pages/main.js
@@ -4,16 +4,20 @@ define(["jquery", "PoziMap", "proj", "pages/details"], function($, PoziMap, proj
     var result = {
         init: function() {
             window.map = new PoziMap();
-            $("#toSyncButton").closest("div").hide();
             $page.on("pagebeforeshow", function() { map.updateData(); });
             $("#zoomOut").click(function() { map.zoomOut(); });
             $("#seekToCurrentLocation").click(function() { map.seekToCurrentLocation(); });
             $("#zoomIn").click(function() { map.zoomIn(); });
             $("#newButton").click(function() { details.new(map.getCenterInWGS84()).changeTo(); });
+        },
+
+        setSyncButton: function(icon, number) {
+            var newButton = '<button id="syncButton" class="ui-btn-left" data-icon="'+icon+'">'+number+'</button>';
+            $("#syncButton").closest("div").remove();
+            $page.find("header").prepend(newButton).trigger("create");
         }
     };
 
     return result;
-
 });
  
