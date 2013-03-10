66df635222dd1e244e53d9b40d112077aff5137f
Chris Berkhout <chrisberkhout@gmail.com>
2013-03-10 16:42:06 +1100

    Filled out Pages spec.

--- js/spec/PagesSpec.js
+++ js/spec/PagesSpec.js
@@ -1,7 +1,30 @@
-define(["spec/SpecHelper", "Pages"], function(SpecHelper, Pages) {
-// 
-//     describe("Pages", function() {
-//     });
-// 
+define(["spec/SpecHelper", "Pages", "pages/Main"], function(SpecHelper, Pages, Main) {
+
+    describe("Pages", function() {
+        var subject, main, details, syncher;
+
+        beforeEach(function() {
+            spyOn(Main, "doNew").andReturn(main = jasmine.createSpyObj("pages", ["setSyncButton", "updateData"]));
+            subject = Pages.doNew(); 
+        });
+
+        describe("#setSyncButton", function() {
+            it("should delegate to main page", function() {
+                var icon = jasmine.createSpy("icon");
+                var label = jasmine.createSpy("label");
+                subject.setSyncButton(icon, label);
+                expect(main.setSyncButton).toHaveBeenCalledWith(icon, label);
+            });
+        });
+
+        describe("#updateData", function() {
+            it("should delegate to main page", function() {
+                subject.updateData();
+                expect(main.updateData).toHaveBeenCalledWith();
+            });
+        });
+
+    });
+
 });
 
