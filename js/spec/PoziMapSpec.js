35bd3591b65b42b2999e2ba2e92e28a7e70fefc4
Chris Berkhout <chrisberkhout@gmail.com>
2013-03-15 15:55:49 +1100

    Added some pending specs for PoziMap.

--- js/spec/PoziMapSpec.js
+++ js/spec/PoziMapSpec.js
@@ -1,15 +1,49 @@
-define(["spec/SpecHelper", "PoziMap"], function(SpecHelper, PoziMap) {
+define(["spec/SpecHelper", "PoziMap", "layers", "openlayers"], function(SpecHelper, PoziMap, layers, OpenLayers) {
 
     describe("PoziMap", function() {
         var subject, detailsPage;
+        
+        // TODO: make PoziMap a wrapper not a subclass of OpenLayers.Map (then it can be better isolated in these specs, etc.).
 
         beforeEach(function() {
-          // setFixtures('<div id="map"></div>');
+          setFixtures('<div id="map"></div>');
+          spyOn(layers.data, "getFeaturesAround");
           detailsPage = jasmine.createSpyObj("detailsPage", ["update", "changeTo"]);
           subject = PoziMap.doNew(detailsPage);
         });
 
-        it("should work", function() {
+        it("should have meters as map units", function() {
+            expect(subject.units).toEqual("m");
+        });
+        it("should set default zoom level", function() {
+            expect(subject.zoom).toEqual(15);
+        });
+
+        describe("#seekToCurrentLocation", function() {
+            it("should clear location markers before getting new location", function() {
+            });
+            it("should getCurrentLocation() if already active", function() {
+            });
+            it("should activate gelocaation if not already active", function() {
+            });
+        });
+
+        describe("#setSize", function() {
+            it("should be set on initialization", function() {
+            });
+            it("should be set on resize", function() {
+            });
+            it("should be set on orientationchange", function() {
+            });
+            it("should be set on end of pan", function() {
+            });
+        });
+
+        describe("#updateData",function() {
+            it("should be done on initialization", function() {
+            });
+            it("should delegate to data layer with correct center point", function() {
+            });
         });
 
     });
