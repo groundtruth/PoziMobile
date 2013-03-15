eeab888b00907d7e4bf958ea8614941100d0ea5f
Chris Berkhout <chrisberkhout@gmail.com>
2013-03-15 16:22:23 +1100

    Implemented some specs.

--- js/spec/PoziMapSpec.js
+++ js/spec/PoziMapSpec.js
@@ -15,17 +15,40 @@ define(["spec/SpecHelper", "PoziMap", "layers", "openlayers"], function(SpecHelp
         it("should have meters as map units", function() {
             expect(subject.units).toEqual("m");
         });
+
         it("should set default zoom level", function() {
             expect(subject.zoom).toEqual(15);
         });
 
         describe("#seekToCurrentLocation", function() {
+            var geolocate;
+
+            beforeEach(function() {
+                spyOn(layers.currentLocation, "clearLocationMarker");
+                geolocate = jasmine.createSpyObj("geolocate", ["activate", "getCurrentLocation"]);
+                spyOn(subject, "getControlsBy").andReturn([geolocate]);
+            });
+
             it("should clear location markers before getting new location", function() {
+                geolocate.activate.andCallFake(function() {
+                    expect(layers.currentLocation.clearLocationMarker).toHaveBeenCalled();
+                });
+                subject.seekToCurrentLocation();
+                expect(geolocate.activate).toHaveBeenCalled();
             });
+
             it("should getCurrentLocation() if already active", function() {
+                geolocate.active = true;
+                subject.seekToCurrentLocation();
+                expect(geolocate.getCurrentLocation).toHaveBeenCalled();
             });
+
             it("should activate gelocaation if not already active", function() {
+                geolocate.active = false;
+                subject.seekToCurrentLocation();
+                expect(geolocate.activate).toHaveBeenCalled();
             });
+
         });
 
         describe("#setSize", function() {
