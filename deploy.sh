c5cc402e617a0c7a8369291002d4683e7075d41e
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-22 22:35:45 +0200

    Updated deploy.sh to check for uncommitted config changes.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
-ssh root@general.pozi.com "cd /var/lib/tomcat6/webapps/PoziMobile && git pull origin master && cd config && git pull origin master"
+(cd config && git status | grep "nothing to commit") &&
+ssh root@general.pozi.com "cd /var/lib/tomcat6/webapps/PoziMobile && git pull origin master && cd config && git pull origin master" ||
+echo "you have uncommitted changes in config/"
 
