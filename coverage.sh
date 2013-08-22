5f40871d423bde5113691212098e2f27c1deb852
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-22 19:37:51 +0200

    Got coverage working properly again.

--- coverage.sh
+++ coverage.sh
@@ -2,12 +2,14 @@
 
 (
     sleep 1
-    open http://127.0.0.1:8080/jscoverage.html?SpecRunner.html
+    grunt jasmine::build
+    open http://127.0.0.1:8080/jscoverage.html?_SpecRunner.html
 ) &
 
 jscoverage-server \
     --no-instrument=/lib \
-    --no-instrument=/js/spec \
+    --no-instrument=/spec \
+    --no-instrument=.grunt \
     --verbose
 
 
