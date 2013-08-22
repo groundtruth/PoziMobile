20e9a79e32b4eb7c875c630b4f0a8faed89a3d5f
Chris Berkhout <chrisberkhout@gmail.com>
2013-08-22 21:43:09 +0200

    Updated deploy script to also pull latest config.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-ssh root@general.pozi.com "cd /var/lib/tomcat6/webapps/PoziMobile && git pull origin master"
+ssh root@general.pozi.com "cd /var/lib/tomcat6/webapps/PoziMobile && git pull origin master && cd config && git pull origin master"
 
