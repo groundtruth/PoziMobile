9541331331643e35a7c0d5ed7d45b3cb0242249e
Chris Berkhout <chrisberkhout@gmail.com>
2013-09-30 17:09:57 +1000

    Fixed data layer specs.

--- spec/js/layers/dataSpec.js
+++ spec/js/layers/dataSpec.js
@@ -3,17 +3,23 @@ define(["spec/SpecHelper", "js/config", "js/layers/data"], function(SpecHelper, 
     describe("layers/data", function() {
         describe("#getFeaturesAround", function() {
 
-            var configData, lon, lat, handler, responseData;
+            var lon, lat, lonWebMercator, latWebMercator, handler, responseData, configData;
 
             beforeEach(function() {
-                configData = jasmine.createSpyObj("configData", ["readEndpoint", "featuresLimit", "databaseName", "iconName"]);
+                configData = {
+                  restEndpoint: "http://example.com/rest",
+                  featuresLimit: 33,
+                  iconName: "somicon.png"
+                };
                 spyOn(config, "data").andReturn(configData);
                 lon = 143.65305771415987;
                 lat = -36.43791886509164;
-                responseData = { 
+                lonWebMercator = 15990432.821168;
+                latWebMercator = -4361466.021253;
+                responseData = {
                     "type": "FeatureCollection",
                     "features": [
-                        { "Type":"Feature", "properties": { "id":"12", "comments":"msg" }, "geometry": { "type":"Point", "coordinates": [15990432.821168, -4361466.021253] } }
+                        { "Type":"Feature", "properties": { "id":"12", "comments":"msg" }, "geometry": { "type":"Point", "coordinates": [143.65305771415987, -36.43791886509164] } }
                     ]
                 };
                 spyOn($, "getJSON");
@@ -22,16 +28,16 @@ define(["spec/SpecHelper", "js/config", "js/layers/data"], function(SpecHelper, 
             it("should make the right request to the server", function() {
                 data.getFeaturesAround({ lon: lon, lat: lat });
                 expect($.getJSON).toHaveBeenCalled();
-                expect($.getJSON.mostRecentCall.args[0]).toEqual(configData.readEndpoint);
-                expect($.getJSON.mostRecentCall.args[1]).toEqual({ lat: lat, lon: lon, limit: configData.featuresLimit, config: configData.databaseName });
-                expect($.getJSON.mostRecentCall.args[2]).toEqual(jasmine.any(Function));
+                var querySuffix = '/closest/'+lon+'/'+lat+'/limit/'+configData.featuresLimit;
+                expect($.getJSON.mostRecentCall.args[0]).toEqual(configData.restEndpoint+querySuffix);
+                expect($.getJSON.mostRecentCall.args[1]).toEqual(jasmine.any(Function));
             });
 
             describe("success handler", function() {
 
                 beforeEach(function() {
                     data.getFeaturesAround({ lon: lon, lat: lat });
-                    var handler = $.getJSON.mostRecentCall.args[2];
+                    var handler = $.getJSON.mostRecentCall.args[1];
                     handler(responseData, jasmine.createSpy("textStatus"));
                 });
 
@@ -43,19 +49,19 @@ define(["spec/SpecHelper", "js/config", "js/layers/data"], function(SpecHelper, 
                 });
 
                 it("should place the features in the right places", function() {
-                    expect(data.features[0].geometry.x).toEqual(responseData.features[0].geometry.coordinates[0]);
-                    expect(data.features[0].geometry.y).toEqual(responseData.features[0].geometry.coordinates[1]);
+                    expect(data.features[0].geometry.x).toBeCloseTo(lonWebMercator, -4);
+                    expect(data.features[0].geometry.y).toBeCloseTo(latWebMercator, -3);
                 });
 
                 it("should not duplicate features or 'remove' them without destroying them", function() {
                     data.getFeaturesAround({ lon: lon, lat: lat });
-                    $.getJSON.mostRecentCall.args[2](responseData, jasmine.createSpy("textStatus"));
+                    $.getJSON.mostRecentCall.args[1](responseData, jasmine.createSpy("textStatus"));
                     expect(data.features.length).toEqual(1);
 
                     data.getFeaturesAround({ lon: lon, lat: lat });
-                    $.getJSON.mostRecentCall.args[2](responseData, jasmine.createSpy("textStatus"));
+                    $.getJSON.mostRecentCall.args[1](responseData, jasmine.createSpy("textStatus"));
                     expect(data.features.length).toEqual(1);
-                    
+
                 });
 
             });
@@ -64,7 +70,7 @@ define(["spec/SpecHelper", "js/config", "js/layers/data"], function(SpecHelper, 
 
                 it("should not remove features from a previously successful fetch", function() {
                     data.getFeaturesAround({ lon: lon, lat: lat });
-                    var handler = $.getJSON.mostRecentCall.args[2];
+                    var handler = $.getJSON.mostRecentCall.args[1];
                     handler(responseData, jasmine.createSpy("textStatus"));
                     var successfullyFetchedFeatureId = data.features[0].id;
                     data.getFeaturesAround({ lon: lon, lat: lat });
