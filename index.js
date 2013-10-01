c67d360b6f941c192ded0392aa393ad006ee53a7
Chris Berkhout <chrisberkhout@gmail.com>
2013-10-01 17:16:32 +1000

    Workaround for dom ready being fired multiple times (reason for that not known).

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require(["requirejsConfig"], function() {
     require(["jquery", "jquery.mobile", "js/Pages"], function($, jqm, Pages) {
-        $(document).ready(function(){ Pages.doNew(); });
+        $(document).ready(function(){
+            if (!window.startedPoziMobile) {
+                Pages.doNew();
+                window.startedPoziMobile = true;
+            }
+        });
     });
 });
 
