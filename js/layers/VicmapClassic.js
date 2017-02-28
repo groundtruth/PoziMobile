6753cbc27a19d37f1285518aeaa855f9bb2887aa
Simon <hutch120@gmail.com>
2017-03-01 09:49:22 +1100

    PoziMoble Basemaps get data via PoziProxy and AWS cache

--- js/layers/VicmapClassic.js
+++ js/layers/VicmapClassic.js
@@ -1,25 +1,21 @@
-define(["openlayers"], function(OpenLayers) {
-
-    return function() {
-
-        this.layer = OpenLayers.Layer.WMS.doNew("Vicmap Classic",
-            [
-                "http://basemap1.pozi.com/geoserver/wms",
-                "http://basemap2.pozi.com/geoserver/wms",
-                "http://basemap3.pozi.com/geoserver/wms",
-                "http://basemap4.pozi.com/geoserver/wms"
-            ],
-            {
-                layers: 'VICMAP_CLASSIC:VicmapClassic',
-                format: 'image/png8',
-                tiled: true
-            },
-            {
-                transitionEffect: 'resize'
-            }
-        );
-
-    };
-
-});
-
+define(['openlayers'], function (OpenLayers) {
+  return function () {
+    this.layer = OpenLayers.Layer.WMS.doNew('Vicmap Classic',
+      [
+        'https://imageproxy.pozi.com/http://basemap1.pozi.com/geoserver/wms',
+        'https://imageproxy.pozi.com/http://basemap2.pozi.com/geoserver/wms',
+        'https://imageproxy.pozi.com/http://basemap3.pozi.com/geoserver/wms',
+        'https://imageproxy.pozi.com/http://basemap4.pozi.com/geoserver/wms'
+      ],
+      {
+        layers: 'VICMAP_CLASSIC:VicmapClassic',
+        format: 'image/png8',
+        tiled: true,
+        authkey: 'openlayers2noauth'
+      },
+      {
+        transitionEffect: 'resize'
+      }
+    )
+  }
+})
--- js/layers/VicmapLabelClassic.js
+++ js/layers/VicmapLabelClassic.js
@@ -1,27 +1,23 @@
-define(["openlayers"], function(OpenLayers) {
-
-    return function() {
-
-        this.layer = OpenLayers.Layer.WMS.doNew("Labels",
-            [
-                "http://basemap1.pozi.com/geoserver/wms",
-                "http://basemap2.pozi.com/geoserver/wms",
-                "http://basemap3.pozi.com/geoserver/wms",
-                "http://basemap4.pozi.com/geoserver/wms"
-            ],
-            {
-                layers: 'VICMAP_CLASSIC:LabelClassic',
-                format: 'image/png8',
-                transparent: 'true'
-            },
-            {
-                isBaseLayer: false,
-                singleTile: true,
-                ratio: 1.5
-            }
-        );
-
-    };
-
-});
-
+define(['openlayers'], function (OpenLayers) {
+  return function () {
+    this.layer = OpenLayers.Layer.WMS.doNew('Labels',
+      [
+        'https://imageproxy.pozi.com/http://basemap1.pozi.com/geoserver/wms',
+        'https://imageproxy.pozi.com/http://basemap2.pozi.com/geoserver/wms',
+        'https://imageproxy.pozi.com/http://basemap3.pozi.com/geoserver/wms',
+        'https://imageproxy.pozi.com/http://basemap4.pozi.com/geoserver/wms'
+      ],
+      {
+        layers: 'VICMAP_CLASSIC:LabelClassic',
+        format: 'image/png8',
+        transparent: 'true',
+        authkey: 'openlayers2noauth'
+      },
+      {
+        isBaseLayer: false,
+        singleTile: true,
+        ratio: 1.5
+      }
+    )
+  }
+})
