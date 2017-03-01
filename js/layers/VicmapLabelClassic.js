f8cd5309c5ea6498890a337f4001d2a02fb7e190
Simon <hutch120@gmail.com>
2017-03-01 12:02:54 +1100

    Remove copies of urls to avoid confusing cloudfront cache.

--- js/layers/VicmapLabelClassic.js
+++ js/layers/VicmapLabelClassic.js
@@ -2,10 +2,7 @@ define(['openlayers'], function (OpenLayers) {
   return function () {
     this.layer = OpenLayers.Layer.WMS.doNew('Labels',
       [
-        'https://imageproxy.pozi.com/http://basemap1.pozi.com/geoserver/wms',
-        'https://imageproxy.pozi.com/http://basemap2.pozi.com/geoserver/wms',
-        'https://imageproxy.pozi.com/http://basemap3.pozi.com/geoserver/wms',
-        'https://imageproxy.pozi.com/http://basemap4.pozi.com/geoserver/wms'
+        'https://imageproxy.pozi.com/http://basemap1.pozi.com/geoserver/wms'
       ],
       {
         layers: 'VICMAP_CLASSIC:LabelClassic',
